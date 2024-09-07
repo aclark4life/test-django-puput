69763b451810e227f081a4bc243ba0ad03240447
Jeffrey Alex Clark <aclark@aclark.net>
2024-09-07 18:41:09 -0400

    Init

--- project.mk
+++ project.mk
@@ -2,3 +2,7 @@
 # Add your custom makefile commands here
 #
 PROJECT_NAME := test-django-mongodb
+
+
+django-install: django-install-default
+	$(PIP_INSTALL) git+ssh://git@github.com/aclark4life/puput#egg=puput 
